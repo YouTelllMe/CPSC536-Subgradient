c536b0daf468c7fb906b4d2defd0d378ccdc8732
marclai <marclai@student.ubc.ca>
2024-12-03 00:33:52 -0800

    added: main subgradient class and basic methods, enum for step types

--- src/subgradient.py
+++ src/subgradient.py
@@ -1,5 +1,55 @@
-def subgradient() -> int:
+import numpy as np
+import matplotlib.pyplot as plt
+from enum import Enum, auto
+
+class Step(Enum): 
+    CONSTANT_SIZE = auto() # constant step; some α
+    CONSTANT_LENGTH = auto() # constant step normalized; some α/||gk||. Then ||x_(k+1)-xk|| = α (?)
+    SQAURE_SUMMABLE_NON_SUMMABLE = auto() # αk >= 0, square summable (finite), not summable (diverge / infinite). Ex: α / (b+k) where a > 0, b >= 0
+    NON_SUMMABLE_DIMINISHING_SIZE = auto() # αk >= 0, limit of ak to 0, not summable (diverge / infinite). Ex: α / sqrt(k)
+    NON_SUMMABLE_DIMINISHING_LENGTH = auto() # step is αk / ||gk|| where αk >= 0, limit is 0, not summable (diverge / infinite).
+
+class SubgradientDescent:
     """
-    Basic Subgradient Method
+    Class for Basic Subgradient Method
     """
-    pass
+
+    def __init__(self, 
+                 x0: np.ndarray,
+                 stepstyle: Step,
+                 f):
+        self.xcur = x0
+        self.stepstyle = stepstyle
+        self.iterations = 0
+        self.f = f
+        self.objectives = np.array([])
+
+    def iterate(self, gk: np.ndarray, alphak: float):
+        """
+            gk: is a subgradient direction
+            alphak: stepsize > 0
+        """
+        self.iterations += 1
+        self.xcur = self.xcur - alphak * gk
+        np.append(self.objectives, self.get_objective())
+
+    def get_objective(self):
+        return self.f(self.xcur)
+
+    def print_statistics(self):
+        print("Hello!")
+
+    def plot(self):
+        plt.figure()
+        plt.plot(range(self.iterations), self.objectives)
+        plt.savefig("plot.png")
+
+class ProjectedSubgradientDescent(SubgradientDescent):
+    def __init__(self, x0, stepstyle, f):
+        super().__init__(x0, stepstyle, f)
+
+class StochasticSubgradientDescent(SubgradientDescent):
+    def __init__(self, x0, stepstyle, f):
+        super().__init__(x0, stepstyle, f)
+
+

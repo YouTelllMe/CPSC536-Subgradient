632ae8850e4b758d0fb309193ff8143020a0b8a1
marclai <marclai@student.ubc.ca>
2024-12-17 22:39:20 -0800

    upload legacy jupyter notebook

--- src/basic.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from methods.subgradient import SubgradientDescent, Step
-
-problem = SubgradientDescent((0, 0), Step.CONSTANT_SIZE, lambda x: np.sqrt(x[0]**2+x[1]**2), 10)
-problem.run((-1, -1), 1)
